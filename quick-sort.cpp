ca941b1cc08124e2dd82c217d2b1f373c7b55f83
Jean Farines <moukira45@gmail.com>
2020-02-19 17:08:20 -0500

    Double pivot quick sort

--- quick-sort.cpp
+++ quick-sort.cpp
@@ -5,8 +5,21 @@
 #include <algorithm>    
 #include <random>       
 #include <chrono>
+#include <tuple>
+
+#include <cassert>
 
 #include <iomanip>
+
+void swap(int* a, int* b);
+int partitionSinglePivot(std::vector<int> *vect, int low, int high);
+void quickSortSinglePivot(std::vector<int> *vect, int low, int high);
+std::tuple<int, int> partitionDoublePivot(std::vector<int> *vect, int low, int high);
+void quickSortDoublePivot(std::vector<int> *vect, int low, int high);
+void printVector(const std::vector<int>* vect, int low, int high);
+void printFullVector(const std::vector<int>* vect);
+std::vector<int>* createRandomArray(int size);
+void createAndSortVect(int size);
 
 void swap(int* a, int* b)
 {
@@ -46,19 +59,85 @@ void quickSortSinglePivot(std::vector<int> *vect, int low, int high)
 	}
 }
 
-void printVector(const std::vector<int>* vect)
+std::tuple<int, int> partitionDoublePivot(std::vector<int> *vect, int low, int high)
 {
-	int size = vect->size();
+	if ((*vect)[low] > (*vect)[high]) // 1st pivot must have the lowest value of the two. If not, swap the two pivots.
+	{
+		swap(&(*vect)[low], &(*vect)[high]);
+	}
+
+	int pivot1 = (*vect)[low]; // We choose the lowest element as the 1st pivot
+	int pivot2 = (*vect)[high]; // We choose the highest element as the 2nd pivot
+
+	int i = low + 1; // cursor for 1st pivot
+	int j = high - 1; // cursor for 2nd pivot
+
+	for (int k = i; k <= j; k++)
+	{
+		if ((*vect)[k] < pivot1) // If element is lower than 1st pivot, swap it in the 1st section
+		{
+			swap(&(*vect)[k], &(*vect)[i]);
+			i++;
+		}
+		else if ((*vect)[k] >= pivot2)
+		{
+			// If element is greater than 2nd pivot, search for the first element to the left that is not greater than the pivot, and swap our element with this one
+			while ((*vect)[j] > pivot2 && k < j)
+				j--;
+			swap(&(*vect)[k], &(*vect)[j]);
+			j--;
+
+			// Then, if the element we just swapped appends to be lower than the 1st pivot, we swap it aswell
+			if ((*vect)[k] < pivot1)
+			{
+				swap(&(*vect)[k], &(*vect)[i]);
+				i++;
+			}
+		}
+	}
+	i--;
+	j++;
+
+	swap(&(*vect)[i], &(*vect)[low]);
+	swap(&(*vect)[j], &(*vect)[high]);
+	return {i, j};
+}
+
+void quickSortDoublePivot(std::vector<int> *vect, int low, int high)
+{	
+	if (low < high)
+	{
+		auto pivot = partitionDoublePivot(vect, low, high);
+		int pivot1 = std::get<0>(pivot);
+		int pivot2 = std::get<1>(pivot);
+
+		// Divide the sort by cutting the array in half and solve it via reccursion.
+		quickSortDoublePivot(vect, low, pivot1 - 1);
+		quickSortDoublePivot(vect, pivot1 + 1, pivot2 - 1);
+		quickSortDoublePivot(vect, pivot2 + 1, high);
+	}
+}
+
+void printVector(const std::vector<int>* vect, int low, int high)
+{
+	low = std::max(0, low);
+	high = std::min(static_cast<int>(vect->size()), high);
 		
-	for (int i = 0; i < size; i++) {
+	for (int i = low; i < high; i++) {
 		std::cout << (*vect)[i] << " ";
 	}
 		
 	std::cout << std::endl;
 }
 
-std::vector<int>* createRandomArray(int size) {
+void printFullVector(const std::vector<int>* vect)
+{
+	int size = static_cast<int>(vect->size());
+	printVector(vect, 0, size);
+}
 
+std::vector<int>* createRandomArray(int size)
+{
 	std::vector<int>* newArray = new std::vector<int>(size);
 	for (int i = 0; i < size; i++)
 	{
@@ -66,52 +145,73 @@ std::vector<int>* createRandomArray(int size) {
 	}
 
 	// Shuffle the array
-	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	unsigned seed = static_cast<unsigned>(std::chrono::system_clock::now().time_since_epoch().count());
 	shuffle((*newArray).begin(), (*newArray).end(), std::default_random_engine(seed));
 
 	return newArray;
 }
 
-void createAndSortVectSinglePivot(int size) {
-
+void createAndSortVect(int size)
+{
 	if (size <= 0) {
 		return;
 	}
 
-	
 	std::cout << "VECTOR OF SIZE " << size << " :" << std::endl;
 
 	std::vector<int> *vect = createRandomArray(size);
 
-	std::cout << "Sorting via Single Pivot Quick-Sort..." << std::endl;
+	{
 
-	auto start = std::chrono::steady_clock::now();
+		std::vector<int> tmp(*vect);
 
-	quickSortSinglePivot(vect, 0, size - 1);
+		std::cout << "Sorting via Single Pivot Quick-Sort..." << std::endl;
 
-	auto end = std::chrono::steady_clock::now();
+		auto start = std::chrono::steady_clock::now();
 
-	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+		quickSortSinglePivot(&tmp, 0, size - 1);
 
-	std::cout << "Sort completed, execution time : " << duration.count() << " ms." << std::endl;
+		auto end = std::chrono::steady_clock::now();
 
-	std::cout << std::endl;
+		assert(std::is_sorted(tmp.begin(), tmp.end()));
+
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+		std::cout << "Sort completed, execution time : " << duration.count() << " ms." << std::endl;
+	}
+	
+	{
+		
+		std::vector<int> tmp(*vect);
+
+		std::cout << "Sorting via Double Pivot Quick-Sort..." << std::endl;
+
+		auto start = std::chrono::steady_clock::now();
+
+		quickSortDoublePivot(&tmp, 0, size - 1);
+
+		auto end = std::chrono::steady_clock::now();
+		
+		assert(std::is_sorted(tmp.begin(), tmp.end()));
+
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+		std::cout << "Sort completed, execution time : " << duration.count() << " ms." << std::endl;
+	}
+
+	std::cout << "-------------------------------" << std::endl << std::endl;
+
+	delete vect;
 }
 
 int main()
 {
-	std::cout << "--- SINGLE PIVOT QUICK-SORT ---" << std::endl << std::endl;
-
-	// --- CREATE AND SORT THREE VECTOR WITH SINGLE PIVOT QUICK-SORT ---
-	createAndSortVectSinglePivot(500);
-	createAndSortVectSinglePivot(35000);
-	createAndSortVectSinglePivot(1000000);
-	// -----------------------------------------------------------------
-	std::cout << "-------------------------------" << std::endl << std::endl;
-
-	while (true) {
-
-	}
+	// --- CREATE AND SORT THREE VECTOR WITH QUICK-SORT ---
+	createAndSortVect(1000);
+	createAndSortVect(100000);
+	createAndSortVect(10000000);
+	createAndSortVect(1000000000);
+	// ----------------------------------------------------
 
 	return 0;
 }
